4ac8273b440f98ac5ca8585bf077b95e5269e8ef
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-12-09 12:00:39 -0500

    CHANGES TO INDEX: updated name of page btwn title tags, added span tags around RGB; CHANGES TO JS: removed test alerty script, added an array of colors, added a var to hold the nodeList of divs/squares, added a var to hold the selected color, for loop to interate through the list of divs/squares and assign each a color from the colors array; CHANGES TO CSS: commented out the the background color used to style the divs/squares

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("script is connected");
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+];
+
+var colorSquares = document.querySelectorAll(".square"),
+selectedColor = colors[3];
+
+for(var i = 0; i < colorSquares.length; i++){
+    colorSquares[i].style.backgroundColor = colors[i];
+}
