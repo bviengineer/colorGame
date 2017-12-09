ed63b538ca1c0326d73ced32ed53cacae147bc5e
BVIDev <16726593+bviengineer@users.noreply.github.com>
2017-12-09 14:53:24 -0500

    updated name of selectedColor var to pcSelectedColor; assinged span tag to a var; gave span tag the color value selected by pc; created var to hold user click event; added condition to for loop that will alert whether the user selection is right or wrong

--- script.js
+++ script.js
@@ -8,8 +8,26 @@ var colors = [
 ];
 
 var colorSquares = document.querySelectorAll(".square"),
-selectedColor = colors[3];
+pcSelectedColor = colors[3],
+colorValueInSpan = document.getElementById("color-in-span");
+
+//Updates the name of the color after the correct guess
+colorValueInSpan.textContent = pcSelectedColor;
 
 for(var i = 0; i < colorSquares.length; i++){
+    //assigns a color to each square
     colorSquares[i].style.backgroundColor = colors[i];
+
+    //compares the color value of the clicked square to the color value selected
+    colorSquares[i].addEventListener("click", function(){
+        var userSelectedColor = this.style.backgroundColor;
+      
+        if(userSelectedColor === pcSelectedColor){
+            alert("You are correct")
+        } else {
+            alert("WRONG!");
+        }
+        console.log(userSelectedColor);
+        // this.style.background
+    });
 }
